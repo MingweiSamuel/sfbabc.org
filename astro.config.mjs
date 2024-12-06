064f3f14096b61d3a05a35ed57cc714faa5bcf31
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-12-06 09:26:41 -0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
   }),
+
   image: {
     remotePatterns: [{ protocol: "https" }],
   },
+
+  site: 'https://bench.builders',
+  integrations: [sitemap()],
 });
